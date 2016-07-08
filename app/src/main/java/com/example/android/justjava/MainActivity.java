212631a57bb2a4317795a1dcf0872c11bf7b3ef7
sodacarbon <sodacarbon@gmail.com>
2016-07-09 00:37:38 +0700

    Quiz: Hook Up Two Buttons https://classroom.udacity.com/courses/ud837/lessons/4038208680/concepts/42913549310923

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -35,11 +35,29 @@ public class MainActivity extends ActionBarActivity {
     /**
      * This method is called when the order button is clicked.
      */
-    public void submittOrder(View view) {
+    public void submitOrder(View view) {
 
         int quantity = 2;
         display(quantity);
         displayPrice(quantity * 5);
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+
+        int quantity = 3;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+
+        int quantity = 1;
+        display(quantity);
     }
 
     /**
